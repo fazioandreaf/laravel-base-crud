6ca8f5e60e1627eac8786d88bcea107eb0181e8a
fazioandrea.f <fazioandrea.f@gmail.com>
2021-05-25 15:33:27 +0200

    connession db controllere e view

---
+++ app/Http/Controllers/controller_db.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controller_db extends Controller
+{
+    function home(){
+        return view('pages.home');
+    }
+}
