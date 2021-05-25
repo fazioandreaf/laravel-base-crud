1122e072c4dad7c1c2149004f235e36f13337727
fazioandrea.f <fazioandrea.f@gmail.com>
2021-05-25 15:45:28 +0200

    lista di nomi e cognomi degli ospiti

--- app/Http/Controllers/controller_db.php
+++ app/Http/Controllers/controller_db.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Ospiti;
 
 use Illuminate\Http\Request;
 
 class controller_db extends Controller
 {
     function home(){
-        return view('pages.home');
+        $ospiti=Ospiti::all();
+        // dd($ospiti);
+        return view('pages.home',compact('ospiti'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,1 +1,18 @@
 @extends('layouts.main_layout')
+@section('content')
+@foreach ($ospiti as $item)
+<ul>
+    <li>
+
+        <a href="">
+
+            {{$item->name}}
+            {{$item->lastname}}
+        </a>
+    </li>
+</ul>
+
+@endforeach
+
+
+@endsection
